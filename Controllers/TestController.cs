70737f2e772344ec59de30932450b0a7af017c58
Allan Garcia <aggarciaa@bancatlan.hn>
2026-06-10 13:42:08 -0600

    Change version to 2.0

--- Controllers/TestController.cs
+++ Controllers/TestController.cs
@@ -12,7 +12,7 @@ public class TestController : ControllerBase
         return Ok(new
         {
             mensaje = "Hola Kubernetes",
-            version = "3.0"
+            version = "2.0"
         });
     }
 }
--- obj/Debug/net10.0/Api-productos.AssemblyInfo.cs
+++ obj/Debug/net10.0/Api-productos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api-productos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c0fc530ebd2807622cf68dc7a56b44fb08ec5ad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6bfb45ed1c6c6774c697018704283d39453a013")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api-productos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api-productos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
